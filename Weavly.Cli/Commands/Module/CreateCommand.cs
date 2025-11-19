239b9850c4c908e5f52ed509bedec7025be7cd9b
phrkdll <b.hotop@proton.me>
2025-11-19 15:25:22 +0000

    Basic functionality for "module create"

--- Weavly.Cli/Commands/Module/CreateCommand.cs
+++ Weavly.Cli/Commands/Module/CreateCommand.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel;
+using Spectre.Console;
 using Spectre.Console.Cli;
 
 namespace Weavly.Cli.Commands.Module;
@@ -11,10 +12,34 @@ public class CreateCommand : InterruptibleAsyncCommand<CreateCommand.Settings>
         [CommandOption("-n|--name <name>")]
         [Description("Module name")]
         public string? ModuleName { get; set; }
+
+        [CommandOption("-t|--testing-framework <name>")]
+        [Description("Testing framework (as listed in 'dotnet new list')")]
+        public string TestingFramework { get; set; } = "xunit";
     }
 
-    public override Task HandleAsync(CommandContext commandContext, Settings settings, CancellationToken ct)
+    public override async Task HandleAsync(CommandContext commandContext, Settings settings, CancellationToken ct)
     {
-        throw new NotImplementedException();
+        var solutionName =
+            Directory.GetFiles(".", "*.sln*").Select(Path.GetFileNameWithoutExtension).FirstOrDefault()
+            ?? throw new InvalidOperationException("No solution file found in the current directory.");
+
+        var moduleName =
+            settings.ModuleName
+            ?? await new TextPrompt<string>("Please enter a module name:").ShowAsync(AnsiConsole.Console, ct);
+
+        var fullModuleName = $"{solutionName}.{moduleName}";
+        var modulePath = Path.Combine("Modules", moduleName, fullModuleName);
+
+        await Runner
+            .WithMessage($"Creating module [teal]{moduleName}[/]...\n")
+            .RunAsync("dotnet", $"new classlib -o {modulePath}.Shared", ct);
+
+        await Runner.RunAsync("dotnet", $"new classlib -o {modulePath}", ct);
+
+        await Runner.RunAsync("dotnet", $"new {settings.TestingFramework} -o {modulePath}.Tests", ct);
+
+        // TODO: Add project references and solution additions
+        // + Remove unnecessary entries from .csproj files
     }
 }
