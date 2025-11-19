c3bc6445d944531265f2afe78afac8dd74a97f35
phrkdll <b.hotop@proton.me>
2025-11-19 23:21:34 +0000

    Clean up and more

--- Weavly.Cli/Commands/Module/CreateCommand.cs
+++ Weavly.Cli/Commands/Module/CreateCommand.cs
@@ -1,12 +1,16 @@
 using System.ComponentModel;
+using System.Xml.Linq;
 using Spectre.Console;
 using Spectre.Console.Cli;
+using Weavly.Cli.Models;
 
 namespace Weavly.Cli.Commands.Module;
 
 [Description("Initializes a new custom module project for the current solution")]
 public class CreateCommand : InterruptibleAsyncCommand<CreateCommand.Settings>
 {
+    private readonly WeavlyModule coreModule = WeavlyModule.New("Core", "Weavly");
+
     public class Settings : CommandSettings
     {
         [CommandOption("-n|--name <name>")]
@@ -28,18 +32,42 @@ public class CreateCommand : InterruptibleAsyncCommand<CreateCommand.Settings>
             settings.ModuleName
             ?? await new TextPrompt<string>("Please enter a module name:").ShowAsync(AnsiConsole.Console, ct);
 
-        var fullModuleName = $"{solutionName}.{moduleName}";
-        var modulePath = Path.Combine("Modules", moduleName, fullModuleName);
+        var module = WeavlyModule.New(moduleName, solutionName);
+
+        AnsiConsole.Write(new Markup($"Creating module [teal]{module.FullName}[/]...\n"));
+        await Runner
+            .WithMessage($"Adding project [teal]{module.Main}[/]...\n")
+            .RunDotnetAsync($"new classlib -o {module.Main}", ct);
 
         await Runner
-            .WithMessage($"Creating module [teal]{moduleName}[/]...\n")
-            .RunAsync("dotnet", $"new classlib -o {modulePath}.Shared", ct);
+            .WithMessage($"Adding project [teal]{module.Shared}[/]...\n")
+            .RunDotnetAsync($"new classlib -o {module.Shared}", ct);
 
-        await Runner.RunAsync("dotnet", $"new classlib -o {modulePath}", ct);
+        await Runner
+            .WithMessage($"Adding project [teal]{module.Tests}[/]...\n")
+            .RunDotnetAsync($"new {settings.TestingFramework} -o {module.Tests}", ct);
 
-        await Runner.RunAsync("dotnet", $"new {settings.TestingFramework} -o {modulePath}.Tests", ct);
+        await Runner
+            .WithMessage($"Adding project/package references...\n")
+            .RunDotnetAsync($"reference add {module.Shared} --project {module.Main}", ct);
+        await Runner.RunDotnetAsync($"reference add {module.Main} {module.Shared} --project {module.Tests}", ct);
 
-        // TODO: Add project references and solution additions
+        if (solutionName == coreModule.Solution)
+        {
+            await Runner.RunDotnetAsync($"reference add {coreModule.Main} --project {module.Main}", ct);
+            await Runner.RunDotnetAsync($"reference add {coreModule.Shared} --project {module.Shared}", ct);
+        }
+        else
+        {
+            await Runner.RunDotnetAsync($"package add {coreModule.Main.FullName} --project {module.Main}", ct);
+            await Runner.RunDotnetAsync($"package add {coreModule.Shared.FullName} --project {module.Shared}", ct);
+        }
+
+        await Runner
+            .WithMessage($"Adding projects to solution...\n")
+            .RunDotnetAsync($"sln add {module.Main} {module.Shared} {module.Tests}", ct);
+
+        // TODO: Clean up project files (e.g., remove default Class1.cs, adjust namespaces, etc.)
         // + Remove unnecessary entries from .csproj files
     }
 }
