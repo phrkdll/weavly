854a1d9bf3712e3499fcbbd44dfc19117ae0475e
phrkdll <b.hotop@proton.me>
2025-11-24 22:31:37 +0000

    Add template for Weavly modules

---
+++ Modules/Template/WeavlyTemplate.Tests/UnitTest1.cs
@@ -0,0 +1,7 @@
+﻿namespace WeavlyTemplate.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1() { }
+}
---
+++ Modules/Template/WeavlyTemplate/WeavlyTemplateModule.cs
@@ -0,0 +1,11 @@
+﻿namespace WeavlyTemplate;
+
+public class WeavlyTemplateModule : WeavlyTemplate
+{
+    public override void Configure(WebApplicationBuilder builder)
+    {
+        // Configure services here
+
+        base.Configure(builder);
+    }
+}
