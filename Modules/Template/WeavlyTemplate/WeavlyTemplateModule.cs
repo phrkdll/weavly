be8d2124c8c77abfafb262e64a8ca4b40b991bc8
phrkdll <b.hotop@proton.me>
2025-11-25 09:06:18 +0000

    Update module template

--- Modules/Template/WeavlyTemplate/WeavlyTemplateModule.cs
+++ Modules/Template/WeavlyTemplate/WeavlyTemplateModule.cs
@@ -1,11 +1,27 @@
-﻿namespace WeavlyTemplate;
+﻿using Microsoft.AspNetCore.Builder;
 
-public class WeavlyTemplateModule : WeavlyTemplate
+namespace WeavlyTemplate;
+
+public class WeavlyTemplateModule : WeavlyModule
 {
     public override void Configure(WebApplicationBuilder builder)
     {
-        // Configure services here
+        // Module specific configuration can be added here
 
         base.Configure(builder);
+    }
+
+    public override void Use(WebApplication app)
+    {
+        // Module specific middleware can be added here
+
+        base.Use(app);
+    }
+
+    public override Task InitializeAsync()
+    {
+        // Module specific initialization can be added here
+
+        return base.InitializeAsync();
     }
 }
