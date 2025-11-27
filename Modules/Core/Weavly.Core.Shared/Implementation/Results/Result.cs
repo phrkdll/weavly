489317e86718d9a0d11f4f06e9acdc483a14f35b
Benjamin <b.hotop@proton.me>
2025-11-27 12:04:04 +0100

    Remove suppression annotations from Result and Success class (#10)

--- Modules/Core/Weavly.Core.Shared/Implementation/Results/Result.cs
+++ Modules/Core/Weavly.Core.Shared/Implementation/Results/Result.cs
@@ -1,8 +1,5 @@
-using System.Diagnostics.CodeAnalysis;
-
 namespace Weavly.Core.Shared.Implementation.Results;
 
-[SuppressMessage("ReSharper", "AutoPropertyCanBeMadeGetOnly.Global")]
 public class Result
 {
     protected Result() { }
